b06c0a45d6f9da0ac3e3e388bfddec7ffdebb7bd
Quentin Miraud <qumiraud@2A5.42angouleme.fr>
2025-07-25 10:39:37 +0200

    version commentée en fr

--- picoshell.c
+++ picoshell.c
@@ -6,7 +6,7 @@
 /*   By: qumiraud <qumiraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 13:26:33 by qumiraud          #+#    #+#             */
-/*   Updated: 2025/07/24 16:53:18 by qumiraud         ###   ########.fr       */
+/*   Updated: 2025/07/25 10:38:57 by qumiraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,9 +85,7 @@ int	picoshell(char **args[])
 		else
 		{
 			if (prevfd != -1)
-			{
 				close(prevfd);
-			}
 			if (args[i + 1])
 			{
 				close(pipefd[1]);
@@ -105,6 +103,95 @@ int	picoshell(char **args[])
 		;
 	if (status != 0)
 		status = 1;
-	printf("status: %d\n", status);
+	// printf("status: %d\n", status); //Pour verifier avec les tests de la valeur de retour
 	return (status);
+}
+
+
+
+
+
+int	picoshell(char **args[])
+{
+	int		i;
+	int		status;
+	int		pipefd[2];
+	int		prevfd;
+	pid_t	pid;
+
+	i = 0;
+	status = 0;
+	prevfd = -1;
+	//Verification qu'il y est au moins un argument
+	if (args[i][0] == NULL)
+		return (0);
+	while (args[i])
+	{
+		//Création d'un nouveau pipe a chaque nouvelle commande
+		if (pipe(pipefd) == -1)
+		{
+			return (1);
+		}
+		//Creation du processus enfant
+		pid = fork();
+		if (pid == -1)
+			return (1);
+		//Child
+		else if (pid == 0)
+		{
+			//si il y a deja eu une commande
+			if (prevfd != -1)
+			{
+				dup2(prevfd, STDIN_FILENO);
+				close (prevfd);
+			}
+			//si il y a des commandes suivantes (pour ceux qui viendront après)
+			if (args[i + 1])
+			{
+				close(pipefd[0]);
+				dup2(pipefd[1], STDOUT_FILENO);
+				close(pipefd[1]);
+			}
+			//sinon on ne redirige pas la sortie et on ferme bien les fd du pipe
+			else
+			{
+				close(pipefd[0]);
+				close(pipefd[1]);
+			}
+			//execution de la commande
+			if (execvp(args[i][0], args[i]) == -1)
+			{
+				exit (1);
+			}
+		}
+		//Parent
+		else
+		{
+			//si prevfd a été utilisé on le ferme
+			if (prevfd != -1)
+				close(prevfd);
+			//si il y a des commandes aprés, on attribue à prevfd la valeur du fd de pipefd[0]
+			if (args[i + 1])
+			{
+				close(pipefd[1]);
+				prevfd = pipefd[0];
+			}
+			//sinon on ferme tout
+			else
+			{
+				close(pipefd[0]);
+				close (pipefd[1]);
+			}
+			//on passe a la prochaine commande si il y en a une
+			i++;
+		}
+	}
+	//on attend le retour des enfants
+	while(wait(&status) > 0)
+		;
+	//si probleme il y a eu on retourne 1
+	if (status != 0)
+		return (1);
+	//sinon on retourne 0
+	return (0);
 }
